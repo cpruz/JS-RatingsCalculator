c972afd3ac566f5db9fb602db52c42e703b9b687
Colin Pruzek <colin.pruzek@gmail.com>
2019-08-25 15:13:32 -0500

    Finshed Again

--- js/ratings.js
+++ js/ratings.js
@@ -19,5 +19,5 @@ const collect_ratings = () => {
 
 document.addEventListener('change', () => {
   const ratings = collect_ratings();
-  document.querySelectorAll('#average').value = ratings.average.toFixed(2);
+  document.querySelector('#average').value = ratings.average.toFixed(2);
 });
